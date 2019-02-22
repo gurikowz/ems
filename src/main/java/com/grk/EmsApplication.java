9a6b6f2bb91172d877e4164bd9d721f05b0fa5d5
Gurikowz <15515646315@163.com>
2019-02-22 15:55:26 +0800

    G/mgs_v1.0

--- src/main/java/com/grk/EmsApplication.java
+++ src/main/java/com/grk/EmsApplication.java
@@ -1,14 +1,14 @@
 package com.grk;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.mybatis.spring.annotation.MapperScan;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.grk.dao")
 public class EmsApplication {
     public static void main(String[] args) {
-
+        System.out.println("asdf");
 
         SpringApplication.run(EmsApplication.class, args);
     }
