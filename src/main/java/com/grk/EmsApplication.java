5d4482fb1cb66c1e45e43d4e0e165af9a907cbeb
Gurikowz <15515646315@163.com>
2019-02-22 11:50:21 +0800

    v2.0

--- src/main/java/com/grk/EmsApplication.java
+++ src/main/java/com/grk/EmsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.grk.dao")
 public class EmsApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(EmsApplication.class, args);
     }
 }
